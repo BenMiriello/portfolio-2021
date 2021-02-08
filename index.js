c28320cbb6b44078e4d8ada2e7e23151529a2977
BenMiriello <benmiriello@gmail.com>
2021-02-08 13:32:41 -0500

    Saving a basic homepage version before reworking card tag hierarchy

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const pc1 = document.getElementById('project-card-1');
 pc1.addEventListener('click', () => {
-  pc1.classList.toggle('project-card-extended')
+  pc1.classList.toggle('project-card-extended');
+  pc1.classList.toggle('project-card-1-extended');
 });
