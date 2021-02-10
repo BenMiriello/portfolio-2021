79a0bcfb65c0f1bbd6e1d2a8c0d1fed8c6faa687
BenMiriello <benmiriello@gmail.com>
2021-02-09 21:17:03 -0500

    Change homepage card tag hierarchy w/ overflow:hidden containing a bigger gradient bg

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+const pcc1 = document.getElementById('project-card-container-1');
+
 const pc1 = document.getElementById('project-card-1');
 pc1.addEventListener('click', () => {
-  pc1.classList.toggle('project-card-extended');
-  pc1.classList.toggle('project-card-1-extended');
+  pcc1.classList.toggle('project-card-container-extended');
 });
